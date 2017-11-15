a3771ba2d89428e398d1a8f0ba256d63cd34c145
Luisa Mckenna <Mckennal1@wit.edu>
2017-11-14 23:31:52 -0500

    update with sponsor page

--- js/self.js
+++ js/self.js
@@ -1,26 +1,6 @@
-
-// Scroll button to top of page
-// When the user scrolls down 20px from the top of the document, show the button
-$(document).ready(function()) {
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//         document.getElementById("myBtn").style.display = "block";
-//     } else {
-//         document.getElementById("myBtn").style.display = "none";
-//     }
-// }
-
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-//     document.documentElement.scrollTop = 0; // For IE and Firefox
-// }
-
-  $('.myclick').click(function() {
-    $(this).children('ul').toggle();
-  });
+$(document).ready(function(){
+	$('.hide').hide();
+	$('#button_bio').click(function(){
+		$('#more1').toggle();
+	});
 });
-
-//
